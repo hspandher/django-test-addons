9eef42d1058cc9870a1994e2f6d07c7579a07cfe
hspandher <hspandher@outlook.com>
2015-07-25 23:58:18 +0530

    Fix setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-	name = 'django_test_utils',
-	packages = ['django_test_utils'],
+	name = 'django-test-utils',
+	packages = ['django-test-utils'],
 	version = '0.1',
 	description = 'A random test lib',
 	author = 'Hakampreet Singh Pandher',
 	author_email = 'hspandher@outlook.com',
-	url = '#',
-	download_url = '#',
+	url = 'https://github.com/hspandher/django-test-utils',
+	download_url = 'https://github.com/hspandher/django-test-utils/tarball/0.1',
 	kewords = ['testing', 'django', 'mongo', 'redis', 'neo4j'],
 	classifiers = [],
 )
